044f31dda2fc7037b554f5972eddde29efdfe43f
geniemo <kor98won@gmail.com>
2022-12-08 15:01:19 +0900

    add: create quiz entity method

--- server/src/main/java/wpl/server/entity/Quiz.java
+++ server/src/main/java/wpl/server/entity/Quiz.java
@@ -21,4 +21,12 @@ public class Quiz {
     private String title;
     private String description;
     private Integer answer;
+
+    public static Quiz createQuiz(String title, String description, Integer answer) {
+        Quiz quiz = new Quiz();
+        quiz.setTitle(title);
+        quiz.setDescription(description);
+        quiz.setAnswer(answer);
+        return quiz;
+    }
 }
