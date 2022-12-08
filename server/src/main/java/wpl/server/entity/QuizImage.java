2330fb094e2329669670045d50102a6e6d0aa0d8
geniemo <kor98won@gmail.com>
2022-12-08 15:28:41 +0900

    add: quiz image entity

---
+++ server/src/main/java/wpl/server/entity/QuizImage.java
@@ -0,0 +1,36 @@
+package wpl.server.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import wpl.server.file_storage.File;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+public class QuizImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "quiz_image_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "file_id")
+    private File file;
+
+    public static QuizImage createQuizImage(Quiz quiz, File file) {
+        QuizImage quizImage = new QuizImage();
+        quizImage.setQuiz(quiz);
+        quizImage.setFile(file);
+        return quizImage;
+    }
+}
