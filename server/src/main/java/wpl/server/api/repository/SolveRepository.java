1b9f8844cf2f05d1747dc77f992d12b3235c1919
geniemo <kor98won@gmail.com>
2022-12-11 00:02:20 +0900

    delete: unused solve repository query method

--- server/src/main/java/wpl/server/api/repository/SolveRepository.java
+++ server/src/main/java/wpl/server/api/repository/SolveRepository.java
@@ -1,12 +1,7 @@
 package wpl.server.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import wpl.server.entity.Quiz;
 import wpl.server.entity.Solve;
-import wpl.server.entity.User;
-
-import java.util.Optional;
 
 public interface SolveRepository extends JpaRepository<Solve, Long> {
-    Optional<Solve> findByUserAndQuiz(User user, Quiz quiz);
 }
